df8ab06ef5b35c465eb4a06003fb042b7643dfb2
Mike Wernicke <mike.wernicke@gmail.com>
2017-09-20 14:30:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ function deleteFromObjectByKey(object, key) {
   return delete newObject.key
 }
 function destructivelyDeleteFromObjectByKey(object, key) {
-  return delete object.key
+  return delete object[key]
 }
