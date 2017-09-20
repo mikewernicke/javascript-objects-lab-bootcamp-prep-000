2aba3bc2a458a1fd4bdc0985cf8f2e3e07e26a36
Mike Wernicke <mike.wernicke@gmail.com>
2017-09-20 14:03:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var recipes = {};
 
-//function updateObjectWithKeyandValue(object, key, value) {
+function updateObjectWithKeyandValue(object, key, value) {
 //  return Object.assign({}, object, { [key] : value})
-//}
+}
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object
